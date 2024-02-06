1550683074af594d7d2f29e9e2010b782eb656ba
F-Yousefi <farzad.yousefi76gmail@example.com>
2024-02-06 05:46:55 -0800

    Everything is Working
    In the current version, we have trained our model and
    with maximum mAP saved it. I additionally loaded the
    model and evaluated it again to make sure everything
    is working. Nex step is to perform similar training on the
    model proposed by the author of retinanet article.

--- scratch.py
+++ scratch.py
@@ -1,5 +1,4 @@
 from typing import Any
-
 from pytorch_lightning.utilities.types import EVAL_DATALOADERS, STEP_OUTPUT, TRAIN_DATALOADERS, OptimizerLRScheduler
 from torch import Tensor
 from data.charlotte_dataset import generate_charlotte_dataloader
@@ -23,14 +22,8 @@ class CharlotteDetector(L.LightningModule):
         trainable_backbone_layers = 0
         self.model = get_efficient_retinanet(num_classes=num_classes, 
                                             efficientnet_model_name=model_name, 
-                                            trainable_backbone_layers=trainable_backbone_layers)
-        # self.train_split_dataloader, self.val_split_dataloader, test = generate_charlotte_dataloader(
-        #                                                 batch_size= self.batch_size, 
-        #                                                 num_workers=4, 
-        #                                                 train_frac=0.90, 
-        #                                                 val_frac=0.099, 
-        #                                                 test_frac=0.001)
-
+                                            trainable_backbone_layers=trainable_backbone_layers,
+                                            score_thresh=0.4).to("cuda")
         self.mean_average_precision  = MeanAveragePrecision()
 
     def training_step(self, batch) -> STEP_OUTPUT:
@@ -52,21 +45,26 @@ class CharlotteDetector(L.LightningModule):
     
 
     def validation_step(self, batch) -> STEP_OUTPUT:
-        # pass
-        images, targets = self.to_cuda(batch)
-        self.model.eval()
-        detections = self.model(images)
-        # mAP = self.mean_average_precision(detections, targets)
-        # self.log_dict({"validation mAP":mAP["map"]}, prog_bar=True, on_step=True, batch_size=self.batch_size)
+        images, targets = self.to_cuda(batch=batch)
+        detections = [{key: value.detach().cpu() for key, value in image.items()}  for image in self.model(images)]
+        targets = [{key: value.detach().cpu() for key, value in image.items()}  for image in targets]
+        mAP = self.mean_average_precision(detections, targets)
+        self.log("validation_mAP", mAP["map"], prog_bar=True, on_step=True, on_epoch=True, batch_size=self.batch_size)
+        gpu_temperature(80,55)
+        del detections
+        torch.cuda.empty_cache()
 
-    # def on_validation_epoch_end(self) -> None:
-    #     myTable = PrettyTable(["index", "value"]) 
-    #     mAP = self.mean_average_precision.compute()
-    #     _ = [myTable.add_row([key, values]) for key, values in mAP.items()] 
-    #     print(myTable)
-    #     self.mean_average_precision.reset()
-
-
+    def on_validation_epoch_end(self) -> None:
+        myTable = PrettyTable(["index", "value"]) 
+        mAP = self.mean_average_precision.compute()
+        _ = [myTable.add_row([key, f"{values.item():.4f}"]) for key, values in mAP.items() if values.nelement() == 1 ] 
+        print(myTable)
+        self.mean_average_precision.reset()
+    
+    def predict_step(self, images, *args: Any, **kwargs: Any) -> Any:
+        images = images.to("cuda")
+        detections = [{key: value.detach().cpu() for key, value in image.items()}  for image in self.model(images)]
+        return detections
     
 
     def to_cuda(self, batch):
@@ -83,54 +81,29 @@ class CharlotteDetector(L.LightningModule):
     def configure_optimizers(self) -> OptimizerLRScheduler:
         return torch.optim.Adam(self.model.parameters(), lr=1e-05, weight_decay=1e-04)
 
-    # def train_dataloader(self) -> TRAIN_DATALOADERS:
-    #     return self.train_split_dataloader
     
 
-batch_size = 2
-train_split_dataloader, val_split_dataloader, test = generate_charlotte_dataloader(
-                                                batch_size= batch_size, 
-                                                num_workers=4, 
-                                                train_frac=0.90, 
-                                                val_frac=0.099, 
-                                                test_frac=0.001)
+from data.charlotte_dataset import generate_charlotte_dataloader, to_cuda
+batch_size = 4
+train_dataloader , val_dataloader = generate_charlotte_dataloader(train_frac=0.9, 
+                                                                   val_frac=0.1, 
+                                                                   num_workers = 3, 
+                                                                   batch_size = batch_size,
+                                                                   persistent_workers=True)
+
+from pytorch_lightning.callbacks import ModelCheckpoint
+checkpoint_callback = ModelCheckpoint(monitor='validation_mAP', filename="ER_best_mAP.ckpt", mode="max",dirpath="./")
 
 model = CharlotteDetector(batch=batch_size)
-trainer = L.Trainer(max_epochs = 10 , enable_model_summary=True, enable_progress_bar=True )
-trainer.fit(model=model, train_dataloaders=train_split_dataloader, val_dataloaders=val_split_dataloader) 
+trainer = L.Trainer(max_epochs = 20 , enable_model_summary=True, enable_progress_bar=True, callbacks=[checkpoint_callback] )
+trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader) 
 
 
+model =CharlotteDetector.load_from_checkpoint("ER_best_mAP.ckpt", batch=batch_size)
 
+print(model.learning_rate)
+# prints the learning_rate you used in this checkpoint
 
-
-# def to_cuda(batch):
-#     images = []
-#     targets = []
-#     for (image, target) in batch:
-#         image = image.to(config.device)
-#         target["boxes"] = target["boxes"].to(config.device)
-#         target["labels"] = target["labels"].to(config.device)
-#         images.append(image)
-#         targets.append(target)
-#     return images, targets
-
-# batch_size = 2
-# num_classes = 7
-# model_name = "efficientnet-b4"
-# trainable_backbone_layers = 0
-# model = get_efficient_retinanet(num_classes=num_classes, 
-#                                     efficientnet_model_name=model_name, 
-#                                     trainable_backbone_layers=trainable_backbone_layers).to("cuda")
-
-
-
-
-
-
-# model.train()
-# images, targets = to_cuda(next(iter(train_split_dataloader)))
-# loss = model(images, targets)
-# model.eval()
-# model.training
-# images, targets = to_cuda(next(iter(val_split_dataloader)))
-# detection = model(images)
+model = CharlotteDetector(batch=batch_size)
+trainer = L.Trainer(max_epochs = 20 , enable_model_summary=True, enable_progress_bar=True, callbacks=[checkpoint_callback] )
+trainer.validate(model, ckpt_path="ER_best_mAP.ckpt", dataloaders=val_dataloader)
